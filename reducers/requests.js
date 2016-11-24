538284ff9051cc50c0f94c730315f58e7ccec492
Kai Bakker <kaibakker@gmail.com>
2016-11-24 23:27:24 +0100

    updates reducers

--- reducers/requests.js
+++ reducers/requests.js
@@ -1,11 +1,13 @@
 const request = (state, action) => {
   if(state == undefined) {
     state = {
-      label: 'hoi',
-      amount: 0.01,
-      address: '124xXJsB7NtjQ8VZEHuTb6aVjb6WjTGjyB',
-      redirect_to_cancel: 'no_redirect',
-      redirect_to_success: 'no_redirect'
+      request: {
+        label: 'hoi',
+        amount: 0.01,
+        address: '124xXJsB7NtjQ8VZEHuTb6aVjb6WjTGjyB',
+        redirect_to_cancel: 'no_redirect',
+        redirect_to_success: 'no_redirect'
+      }
     }
   }
 
@@ -14,7 +16,7 @@ const request = (state, action) => {
       if(action.address == undefined || action.amount == undefined) {
         return state
       }
-      return {
+      request = {
         id: action.id,
         label: action.label,
         amount: action.amount,
@@ -23,37 +25,35 @@ const request = (state, action) => {
         redirect_to_cancel: action.redirect_to_cancel,
         redirect_to_success: action.redirect_to_success
       }
-    case 'UPDATE_N_TX_RECEIVED':
       return Object.assign({}, state, {
-        n_tx_received: action.status
-      })
+        request: request
+      });
+    case 'SET_PANEL_NAME':
+      return Object.assign({}, state, {
+        panel_name: action.panel_name
+      });
     case 'SET_STATUS':
-      if (state.id !== action.id) {
-        return state
-      }
-
       return Object.assign({}, state, {
         status: action.status
-      })
-
+      });
     default:
       return state
   }
 }
 
-const requests = (state = [], action) => {
-  switch (action.type) {
-    case 'NEW_REQUEST':
-      return [
-        request(undefined, action)
-      ]
-    case 'SET_STATUS':
-      return state.map(t =>
-        request(t, action)
-      )
-    default:
-      return state
-  }
-}
+// const requests = (state = [], action) => {
+//   switch (action.type) {
+//     case 'NEW_REQUEST':
+//       return [
+//         request(undefined, action)
+//       ]
+//     case 'SET_STATUS':
+//       return state.map(t =>
+//         request(t, action)
+//       )
+//     default:
+//       return state
+//   }
+// }
 
 module.exports = request;
