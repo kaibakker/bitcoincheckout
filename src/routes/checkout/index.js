12cad550c16c5c922ed90dab53903ad8454fcbc7
Kai Bakker <kaibakker@gmail.com>
2016-12-20 22:16:20 +0100

    Can only access /checkout with request object

--- src/routes/checkout/index.js
+++ src/routes/checkout/index.js
@@ -5,22 +5,36 @@ import BitcoinPanel from "components/checkout/BitcoinPanel"
 import ShapeShiftPanel from "components/checkout/ShapeShiftPanel"
 import CoinbasePanel from "components/checkout/CoinbasePanel"
 
+import makeRequestFromProtocolURI from "utils/get-request-object";
 
+function requireRequest(nextState, replace) {
+    console.log(makeRequestFromProtocolURI().amount == undefined)
+    if (makeRequestFromProtocolURI().amount == undefined) {
+        replace({
+            pathname: '/create',
+            state: { nextPathname: nextState.location.pathname }
+        })
+    }
+}
 // Sync route definition
 export default {
     path: "checkout",
-    component : CheckoutView,
+    component: CheckoutView,
+    onEnter: requireRequest,
     indexRoute: {
         component: IndexPanel,
     },
     childRoutes: [{
         path: "bitcoin_address",
-        component: BitcoinPanel
+        component: BitcoinPanel,
+        onEnter: requireRequest,
     }, {
         path: "shapeshift",
-        component: ShapeShiftPanel
+        component: ShapeShiftPanel,
+        onEnter: requireRequest,
     }, {
         path: "coinbase",
-        component: CoinbasePanel
+        component: CoinbasePanel,
+        onEnter: requireRequest,
     }],
 };
--- src/routes/home/components/home-view.js
+++ src/routes/home/components/home-view.js
@@ -11,12 +11,13 @@ export default class HomeView extends Component {
                     <div className="info">
                         <h1>Bitcheckout.io</h1>
                         <h2>We provide a great bitcoin checkout service, fully opensource</h2>
+
                         <h4>Customers</h4>
                         <p>We provide easy payments on the web, we accept different methods through different providers. With an beautiful and customer friendly interfase.</p>
 
                         <Button bsStyle="info"
                                 componentClass={Link}
-                                to="/checkout?u=bitcoin:124xXJsB7NtjQ8VZEHuTb6aVjb6WjTGjyB?amount=0.03&label=Mastering Bitcoin&redirect_to_cancel=cancel.html&redirect_to_success=success.html">Go to Bitcoin Checkout</Button>
+                                href="/checkout?u=bitcoin:124xXJsB7NtjQ8VZEHuTb6aVjb6WjTGjyB?amount=0.01&label=Mastering Bitcoin&redirect_to_cancel=cancel.html&redirect_to_success=success.html">Go to Bitcoin Checkout</Button>
 
                         <h4>Merchants</h4>
                         <p>We provide an easy way of accepting bitcoin, just a link from your wallet is enough. While you allow customers to pay With a wide foraity of payments. And thus an extra way of making money.</p>
@@ -31,7 +32,7 @@ export default class HomeView extends Component {
 
                         <Button bsStyle="info"
                                 componentClass={Link}
-                                to="/checkout/index?u=testnet:mz5ydTb59nk3AMPrpVtDQi2WGy7F26b8aY?amount=0.03&label=Mastering Bitcoin&redirect_to_cancel=cancel.html&redirect_to_success=success.html">
+                                href="/checkout?u=testnet:mz5ydTb59nk3AMPrpVtDQi2WGy7F26b8aY?amount=0.03&label=Mastering Bitcoin&redirect_to_cancel=cancel.html&redirect_to_success=success.html">
                             Go through testnet checkout
                         </Button>
                     </div>
